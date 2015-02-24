673863830273f27185c344e9e36d56b7c0f99935
HunterC <hunter@irongatecreative.com>
2015-02-24 09:59:25 -0500

    added sponsors, and news sections to front-page.php and added styles

--- wp-content/themes/chamber/front-page.php
+++ wp-content/themes/chamber/front-page.php
@@ -1,9 +1,6 @@
 <?php get_template_part('includes/header'); ?>
 
-<div class="container">
-  <div class="row">
-
-		<section class="theme">
+<section class="theme">
 			<div class="row">
 				<div class="col-xs-8 col-xs-offset-2">
 					<div class="row">
@@ -22,8 +19,77 @@
 					</div>
 				</div>
 			</div>
-		</section>    
-  </div><!-- /.row -->
-</div><!-- /.container -->
+		</section><!-- /section.theme -->
+		
+<section class="news">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 red1">
+				<h4>Featured</h4>
+				<span>
+					<img src="/wp-content/uploads/2015/02/events1.png" alt="">
+				</span>	
+				<strong>Lorem ipsum dolor sit amet, consectetur. span</strong>
+				<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem nulla, deserunt vitae, illum, laborum expedita accusamus perferendis nisi natus quidem dolorum enim quam optio in?
+				</p>
+				<a href="#">View post</a>
+			</div>
+			<div class="col-md-3 blue2">
+				<h4>News</h4>
+				<span>
+					<img src="/wp-content/uploads/2015/02/events2.png" alt="">
+				</span>	
+				<strong>Lorem ipsum dolor sit amet, consectetur. span</strong>
+				<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem nulla, deserunt vitae, illum, laborum expedita accusamus perferendis nisi natus quidem dolorum enim quam optio in?
+				</p>
+				<a href="#">View post</a>
+			</div>
+			<div class="col-md-3 gold3">
+				<h4>Member Highlights</h4>
+				<span>
+					<img src="/wp-content/uploads/2015/02/events3.png" alt="">
+				</span>	
+				<strong>Lorem ipsum dolor sit amet, consectetur. span</strong>
+				<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem nulla, deserunt vitae, illum, laborum expedita accusamus perferendis nisi natus quidem dolorum enim quam optio in?
+				</p>
+				<a href="#">View post</a>
+			</div>
+			<div class="col-md-3 orange4">
+				<h4>Events</h4>
+				<span>
+					<img src="/wp-content/uploads/2015/02/events4.png" alt="">
+				</span>
+				<strong>Lorem ipsum dolor sit amet, consectetur. span</strong>
+				<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem nulla, deserunt vitae, illum, laborum expedita accusamus perferendis nisi natus quidem dolorum enim quam optio in?
+				</p>
+				<a href="#">View post</a>
+			</div>
+		</div>
+	</div>
+</section><!-- /section.news -->
+		
+<section class="sponsors">
+	<div class="container">
+	
+		<h3>Sponsors</h3>
+		<div class="row">
+			<div class="col-sm-4">
+				<img src="/wp-content/uploads/2015/02/firstbank.png" alt="First Bank Richmond">	
+			</div>
+			<div class="col-sm-4">
+				<img src="/wp-content/uploads/2015/02/richmondbaking.png" alt="Richmond Baking">
+			</div>
+			<div class="col-sm-4">
+				<img src="/wp-content/uploads/2015/02/westend.png" alt="West End S.B.">
+			</div>
+		</div><!-- /.row -->
+		
+	</div> <!-- /.container -->
+</section><!-- /section.sponsors -->
+	
 
 <?php get_template_part('includes/footer'); ?>
