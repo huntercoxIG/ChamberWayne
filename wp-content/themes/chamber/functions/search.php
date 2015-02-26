c4fa11dd3297895fdd4af31fa1aee09bad4866e3
HunterC <hunter@irongatecreative.com>
2015-02-26 09:21:34 -0500

    Fixing sub-992px collapsing navbar "collapsing" styles

--- wp-content/themes/chamber/functions/search.php
+++ wp-content/themes/chamber/functions/search.php
@@ -2,7 +2,7 @@
 
 function bst_search_form( $form ) {
     $form = '<form class="form-inline" role="search" method="get" id="searchform" action="' . home_url('/') . '" >
-	<input class="form-control" type="text" value="' . get_search_query() . '" placeholder="' . esc_attr__('Search', 'bst') . '..." name="s" id="s" />
+	<input class="form-control" type="text" value="' . get_search_query() . '" placeholder="' . esc_attr__('search our site', 'bst') . '..." name="s" id="s" />
 	<button type="submit" id="searchsubmit" value="'. esc_attr__('Search', 'bst') .'" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
     </form>';
     return $form;
